135e829386a7a720e207170545fbcf6a56a51aaa
zabalueva <age_eva@mail.ru>
2021-02-04 14:32:41 +0300

    feat: 8/8 passing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = [];
+    if (matrix) {
+        matrix.forEach((item, index) => (index % 2 == 0) ?
+            item.forEach(item => result.push(item)) :
+            result.push(...item.reverse())
+        );
+    }
+	return result;
 }
